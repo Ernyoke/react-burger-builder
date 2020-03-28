08c0769ee5aa412cc547cde96c538d9de6cb1169
Ervin Szilagyi <ernyoke19930115@gmail.com>
2020-03-28 22:56:13 +0200

    Added sidedrawer

--- src/components/Navigation/NaviagationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NaviagationItems/NavigationItem/NavigationItem.js
@@ -4,9 +4,9 @@ import classes from './NavigationItem.module.css';
 
 const navigtionItem = props => (
     <li className={classes.NavigationItem}>
-        <a href={props.link} 
-        className={props.active ? classes.active : null}>
-        {props.children}
+        <a href={props.link}
+            className={props.active ? classes.active : null}>
+            {props.children}
         </a>
     </li>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo.js';
 import NavigationItems from '../NaviagationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-           <NavigationItems /> 
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
